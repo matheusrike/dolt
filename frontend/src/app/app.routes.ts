533b33875d8660168e70887427e454215751fc37
GustavoTorelli <gustavo.torelli17@gmail.com>
2026-01-15 12:22:30 -0300

    feat: zardUI implemented

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: App,
+  },
+  {
+    path: 'home',
+    component: Home,
+  },
+];
